b4eeb9e54825cafe48cfef79ddc6f66ce7db1d7f
Frzz23 <you@example.com>
2024-06-25 09:17:09 +0545

    learned datatypes

---
+++ index.js
@@ -0,0 +1,58 @@
+// // // // // // // // //Variables
+// // // // // // // // //var
+
+// // // // // // // // var x=90;
+// // // // // // // // console.log(x);
+
+// // // // // // // // var x=900;
+// // // // // // // // console.log(x);
+
+// // // // // // // //LET
+// // // // // // // let y=10;
+// // // // // // // console.log(y);
+
+// // // // // // // y=1000;
+// // // // // // // console.log(y);
+
+
+// // // // // // //const
+// // // // // // const a=10;
+// // // // // // console.log(a);
+
+// // // // // // a=20;
+// // // // // // console.log(a);
+
+// // // // // //Data type in js
+// // // // //STring
+// // // // // const output="ram sherpha";
+// // // // // console.log(output);
+// // // // // console.log(typeof output);
+
+// // // // //Number
+// // // // const result=90;
+// // // // const output="40";
+// // // // console.log(result+output);
+// // // // console.log(typeof result);
+
+// // // //undefined
+// // // let y;
+// // // console.log(typeof y);
+
+// // //null
+// // let a=null;
+// // console.log(typeof a);
+
+// // //boolen
+// // const isTrue=true;
+// // console.log(typeof isTrue);
+
+// // //BIGint
+// // const b=344465545n;
+// // console.log(typeof b);
+
+// //SYmbol
+// const out=Symbol("heelo woold");
+// console.log(out);
+// console.log(typeof out);
+
+//
