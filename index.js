0af19c933c777f79e080624cb3949b874eaeac6d
Frzz23 <you@example.com>
2024-06-30 07:53:45 +0545

    basic

--- index.js
+++ index.js
@@ -1,58 +1,142 @@
-// // // // // // // // //Variables
-// // // // // // // // //var
+// // // // // // // // // // // // // // // // // //Variables
+// // // // // // // // // // // // // // // // // //var
 
-// // // // // // // // var x=90;
-// // // // // // // // console.log(x);
+// // // // // // // // // // // // // // // // // var x=90;
+// // // // // // // // // // // // // // // // // console.log(x);
 
-// // // // // // // // var x=900;
-// // // // // // // // console.log(x);
+// // // // // // // // // // // // // // // // // var x=900;
+// // // // // // // // // // // // // // // // // console.log(x);
 
-// // // // // // // //LET
-// // // // // // // let y=10;
-// // // // // // // console.log(y);
+// // // // // // // // // // // // // // // // //LET
+// // // // // // // // // // // // // // // // let y=10;
+// // // // // // // // // // // // // // // // console.log(y);
 
-// // // // // // // y=1000;
-// // // // // // // console.log(y);
+// // // // // // // // // // // // // // // // y=1000;
+// // // // // // // // // // // // // // // // console.log(y);
 
 
-// // // // // // //const
-// // // // // // const a=10;
-// // // // // // console.log(a);
+// // // // // // // // // // // // // // // //const
+// // // // // // // // // // // // // // // const a=10;
+// // // // // // // // // // // // // // // console.log(a);
 
-// // // // // // a=20;
-// // // // // // console.log(a);
+// // // // // // // // // // // // // // // a=20;
+// // // // // // // // // // // // // // // console.log(a);
 
-// // // // // //Data type in js
-// // // // //STring
-// // // // // const output="ram sherpha";
-// // // // // console.log(output);
-// // // // // console.log(typeof output);
+// // // // // // // // // // // // // // //Data type in js
+// // // // // // // // // // // // // //STring
+// // // // // // // // // // // // // // const output="ram sherpha";
+// // // // // // // // // // // // // // console.log(output);
+// // // // // // // // // // // // // // console.log(typeof output);
 
-// // // // //Number
-// // // // const result=90;
-// // // // const output="40";
-// // // // console.log(result+output);
-// // // // console.log(typeof result);
+// // // // // // // // // // // // // //Number
+// // // // // // // // // // // // // const result=90;
+// // // // // // // // // // // // // const output="40";
+// // // // // // // // // // // // // console.log(result+output);
+// // // // // // // // // // // // // console.log(typeof result);
 
-// // // //undefined
-// // // let y;
-// // // console.log(typeof y);
+// // // // // // // // // // // // //undefined
+// // // // // // // // // // // // let y;
+// // // // // // // // // // // // console.log(typeof y);
 
-// // //null
-// // let a=null;
-// // console.log(typeof a);
+// // // // // // // // // // // //null
+// // // // // // // // // // // let a=null;
+// // // // // // // // // // // console.log(typeof a);
 
-// // //boolen
-// // const isTrue=true;
-// // console.log(typeof isTrue);
+// // // // // // // // // // // //boolen
+// // // // // // // // // // // const isTrue=true;
+// // // // // // // // // // // console.log(typeof isTrue);
 
-// // //BIGint
-// // const b=344465545n;
-// // console.log(typeof b);
+// // // // // // // // // // // //BIGint
+// // // // // // // // // // // const b=344465545n;
+// // // // // // // // // // // console.log(typeof b);
 
-// //SYmbol
-// const out=Symbol("heelo woold");
-// console.log(out);
-// console.log(typeof out);
+// // // // // // // // // // //SYmbol
+// // // // // // // // // // const out=Symbol("heelo woold");
+// // // // // // // // // // console.log(out);
+// // // // // // // // // // console.log(typeof out);
 
-//
+// // // // // // // // // // type conversion
+
+// // // // // // // // // // const result ="123";
+// // // // // // // // // // console.log(result);
+// // // // // // // // // // console.log(typeof result);
+// // // // // // // // // // const output = parseInt(result);
+// // // // // // // // // // console.log(output);
+// // // // // // // // // // console.log(typeof output);
+
+// // // // // // // // // // const ex=976;
+// // // // // // // // // // const out =ex.toString();
+// // // // // // // // // // console.log(out);
+// // // // // // // // // // console.log(typeof out);
+
+// // // // // // // // // // const row=false;
+// // // // // // // // // // const ans = String(row);
+// // // // // // // // // // console.log(ans);
+// // // // // // // // // // console.log(typeof ans);
+
+// // // // // // // // // object types
+
+// // // // // // // // const obj={
+// // // // // // // //     name : 'name',
+// // // // // // // //     age : 45,
+// // // // // // // //     married :false,
+    
+// // // // // // // // }
+// // // // // // // // console.log(obj);
+// // // // // // // // console.log(typeof obj);
+
+// // // // // // // const arr =[3,4,5,6,7];
+// // // // // // // console.log(arr);
+// // // // // // // console.log(typeof arr);
+
+
+// // // // // // const date=new Date();
+// // // // // // console.log(date);
+// // // // // // console.log(typeof date);
+
+// // // // // const name ="1";
+// // // // // const no=1;
+// // // // // const obj=Number(name);
+// // // // // console.log(no+obj);
+
+// // // // //String Method
+// // // // const result ="          hello world";
+// // // // console.log(result.length);
+// // // // console.log(result.charAt(0));
+// // // // console.log(result.indexOf("l"));
+// // // // console.log(result.lastIndexOf("l"));
+// // // // console.log(result.toLowerCase());
+// // // // console.log(result.toUpperCase());
+// // // // console.log(result.replace("llo","goood"));
+// // // // console.log(result.includes("nigga"));
+// // // // console.log(result.slice(1,0));
+// // // // console.log(result.concat("Moooooooooooooode"));
+// // // // console.log(result.trim());
+// // // //console.log(result.split(""));
+
+// // // const a=60;
+// // // const b=45;
+// // // const result=a+b;
+// // // console.log("the sum is:"+result);
+// // // console.log('the sum is: $(result)');//templete literal
+
+
+// // let y=90;
+// // y=y+10;
+// // console.log(y);
+
+// // let x=10;
+// // x+=10;
+// // console.log(x);
+
+// let age =50;
+// console.log(age<=10?"fail":'PASS');
+
+
+const obj={
+    name:"John Doe",
+    age: 30,
+}
+
+delete obj.name;
+console.log(obj);
